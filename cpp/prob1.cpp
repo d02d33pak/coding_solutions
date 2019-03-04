056fa791b39a8dbd21a46691f2cc2ef6d71f525a
Deepak Talan <deepak030190@gmail.com>
2019-03-04 20:48:31 +0530

    Update prob1.cpp

--- cpp/prob1.cpp
+++ cpp/prob1.cpp
@@ -1,1 +1,16 @@
-//TO-DO
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map <int, int> myMap;
+        for (int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+            if (myMap.find(diff) != myMap.end()) {
+                return {myMap[diff], i,};
+            }
+            else {
+                myMap.insert(pair <int, int>(nums[i], i));
+            }
+        }
+        return {0,0}; //could do BETTER
+    }
+};
